2aaab6e703ccea63a687d9ae41f9bfa684e9797f
iambhushan6 <bhushan.lokhande19@vit.edu>
2023-10-04 16:02:59 +0530

    any or all rule predicate changes added

--- action.py
+++ action.py
@@ -7,7 +7,7 @@ import main
 with open('./rule.json') as json_file:
     data = json.load(json_file)
     
-
+rule_level_predicate = data["All_or_Any"]
 conditions=data["Conditions"]
 
 string_predicate={
@@ -29,31 +29,46 @@ def take_action(action,mail_ids):
     return True
 
 
+action_mail_ids_map = {condition["Action"]: [] for condition in conditions}
+condition_met_count = 0
+
 for i in conditions:
 
     field_name = i["Field_name"]
     predicate = i["Predicate"]
     value = i["Value"]
     action = i["Action"]
+
     if all([field_name,predicate,value,action]):
         if field_name != "Date":
             value="'%"+value+"%'"
             operator = string_predicate[predicate]
             mail_data=database.select_data(field_name,operator,value)
-            
         else:
             value="-"+value
-
             operator = string_predicate[predicate]
             mail_data=database.date_query(operator,value)
 
         mail_ids=[i[-1] for i in mail_data]
         print(len(mail_ids))
-        take_action(action,mail_ids)
 
-        print("Program has been executed!!!!")
-        print("-------------------------")
-    
+        if mail_ids:
+            condition_met_count += 1
+            action_mail_ids_map[action].extend(mail_ids)
     else:
         print("Check the rule.json script")
         break
+
+if rule_level_predicate == "any":
+    for action, mail_ids in action_mail_ids_map.items():
+        take_action(action,mail_ids)
+elif rule_level_predicate == "all" and condition_met_count == len(conditions):
+    for action, mail_ids in action_mail_ids_map.items():
+        take_action(action,mail_ids)
+else:
+    print("-------------------------")
+    print("All conditions are not met, actions will not be taken.")
+    print("-------------------------")
+
+print("\nProgram has been executed!!!!\n")
+print("-------------------------")
